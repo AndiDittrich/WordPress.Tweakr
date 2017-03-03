01fed7bbf3613a8e35018166766e9678f061ea5e
Andi Dittrich <andi@andidittrich.de>
2017-03-03 19:28:37 +0000

    full js based analytics opt-out

--- class/GoogleAnalytics.php
+++ class/GoogleAnalytics.php
@@ -7,48 +7,19 @@ use \Tweakr\skltn\HtmlUtil as HtmlUtil;
 
 class GoogleAnalytics{
 
-    // analytics tracking snippet
-    // @see https://developers.google.com/analytics/devguides/collection/analyticsjs/
-    const GA_TRACKING_CODE = "
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');";
-    
-    // an additional condition is added to ensure the code is NOT EXECUTED in case a opt-out cookie has been set!
-    // the opt-out is done in js because of possible issues/side effects with caching plugins!!!
-    const WRAPPER_PRE = "(function(b){if(document.cookie.indexOf('tweakr-analytics-optout=')==-1){";
-
-    // in case the opt-out cookie is set, the button text is changed and the css class "tweakr-analytics-out" is added
-    const WRAPPER_POST = "}else{if(!b){return;}b.innerHTML=b.getAttribute('data-text-disabled');b.className='tweakr-analytics-out'}})(document.getElementById('tweakr-analytics-optout'))";
-   
     // add google analytics tracking code
     public function init($trackingID, $anonymizeIP = false){
-        
-        // set opt-out cookie on request - 3650 days
-        if (isset($_POST) && isset($_POST['tweakr-analytics-optout'])){
-            // set optout cookie
-            setcookie('tweakr-analytics-optout', '1', time() + (3600*24*3650));
-        }
-        
-        // generate init code
-        $analyticsInit = array(
-            "ga('create', '". esc_js($trackingID) . "', 'auto');",
-        );
+        // get minified tracking code
+        $code = file_get_contents(TWEAKR_PLUGIN_PATH . '/resources/google-analytics.min.js');
 
-        // use anonymize-ip option ?
-        if ($anonymizeIP){
-            array_push($analyticsInit, "ga('set', 'anonymizeIp', true);");
-        }
+        // strip closing bracket
+        $code = substr($code, 0, -2);
 
-        // send page view
-        array_push($analyticsInit, "ga('send', 'pageview');");
+        // add params
+        $code .= ',\'' . esc_js($trackingID) . '\',' . ($anonymizeIP ? 'true' : 'false') . ');';
 
-        // enqueue code (printed within wp_footer) - close conditional block
-        ResourceManager::enqueueDynamicScript(self::WRAPPER_PRE . self::GA_TRACKING_CODE . implode("\n", $analyticsInit) . self::WRAPPER_POST);
-
-        // ok!
-        return true;
+        // enqueue code (printed within wp_footer)
+        ResourceManager::enqueueDynamicScript($code);
     }
 
     // display user profile (render as html)
@@ -59,24 +30,15 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             'in' => 'Disable Google Analytics',
             'out' => 'Google Analytics has been disabled!',
         ), $attb );
-        
-        // generate form
-        $content = HtmlUtil::generateTag('form', array(
-            'method' => 'post',
-            'action' => $_SERVER['REQUEST_URI']
-        ), false);
-        
+
         // enabled or disabled ? visual user feedback
-        $content .= HtmlUtil::generateTag('button', array(
+        return HtmlUtil::generateTag('button', array(
             'name' => 'tweakr-analytics-optout',
             'id' => 'tweakr-analytics-optout',
-            'type' => 'submit',
+            'type' => 'button',
             'value' => '1',
             'data-text-disabled' => $attb['out'],
         ), true, $attb['in']);
-        
-        // close form tag
-        return $content. '</form>';
     }
 }
 
---
+++ resources/google-analytics.js
@@ -0,0 +1,49 @@
+// Tweakr Analytics Tracking Code with OPT-OUT Button
+(function(optoutButton, trackingID, anonymizeIP){
+    var buttonDisabled = function(){
+        optoutButton.innerHTML = optoutButton.getAttribute('data-text-disabled');
+        optoutButton.className = 'tweakr-analytics-out';
+    };
+
+    // opt-out cookie not set ?
+    if (document.cookie.indexOf('tweakr-analytics-optout=') == -1){
+        // opt-out button visible on current page ?
+        if (optoutButton){
+            // listen to onclick event
+            optoutButton.addEventListener('click', function(evt){
+                evt.preventDefault();
+
+                // create expire date NOW() + 10 Years
+                var expire = new Date();
+                expire.setTime(expire.getTime() + 1000 * 60 * 60 * 24 * 365 * 10);
+
+                // set cookie
+                document.cookie = 'tweakr-analytics-optout=1;path=/;expires=' + expire.toGMTString() + ';';
+
+                // trigger disabled action
+                buttonDisabled();
+            }); 
+        }
+
+        // Google Analytics Code Snippet
+        // @source https://developers.google.com/analytics/devguides/collection/analyticsjs/tracking-snippet-reference
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        // Google Analytics Init
+        ga('create', trackingID, 'auto');
+        ga('set', 'anonymizeIp', anonymizeIP);
+        ga('send', 'pageview');
+
+    // cookie set
+    }else{
+        // change button text
+        if (optoutButton){
+            buttonDisabled();
+        }
+    }
+
+// initialize
+})(document.getElementById('tweakr-analytics-optout'));
